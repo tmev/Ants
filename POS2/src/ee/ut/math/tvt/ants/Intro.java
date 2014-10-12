6cdb3a7bed3572ef81efc5b12d6019339348cb0b
tanel3203 <tanel3203@gmail.com>
2014-10-12 23:47:47 +0300

    Eelmises (POS kaust) on JavaFXiga tehtud. Selles (POS2) on Swingiga tehtud. Tegin ümber, kuna javafxiga oli mul probleeme. Võite ise valida, kumma põhjal teete, kui saate JavaFX korralikult klassidesse jne. Selles on tehtud UI korralikult klassides. Kogu ant osa jm on puudu. Üritasin buildfailiga tegeleda, kuid ei suutnud veel läbi ampsata.

---
+++ POS2/src/ee/ut/math/tvt/ants/Intro.java
@@ -0,0 +1,15 @@
+package ee.ut.math.tvt.ants;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+public class Intro {
+
+	public static void main(String[] args) {
+		IntroUI.introUI();
+	}
+
+}
---
+++ POS2/src/ee/ut/math/tvt/ants/IntroUI.java
@@ -0,0 +1,53 @@
+package ee.ut.math.tvt.ants;
+
+import java.awt.Color;
+import java.awt.GridLayout;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class IntroUI {
+
+	public static void introUI() {
+		JFrame raam = new JFrame("Beerhouse by ANTS"); // raami loomine
+	    raam.setSize(750, 260); // 
+	    raam.setLocation(100, 100); // 
+	    raam.getContentPane().setBackground(Color.WHITE);
+	    raam.setLayout(new GridLayout(1,3)); // paigutushalduri kasutamine
+
+	    JPanel logo = new JPanel(new GridLayout(1, 1));
+	   	JLabel imgLabel = new JLabel(new ImageIcon("beerhouse.jpg"));
+	   	logo.setBackground(Color.WHITE);
+	   	logo.add(imgLabel);
+	    
+	// 
+	   	
+	    JPanel sisu = new JPanel(new GridLayout(1, 2));
+	    JLabel tekst = new JLabel("<html>"
+	    		+ "<b>Team name<br/>"
+	    		+ "Team leader<br/>"
+	    		+ "Team leader email<br/>"
+	    		+ "Team members<br/><b/>"
+	    		+ "Software version number</html>");
+	    sisu.add(tekst);
+	    
+	    JPanel sisu2 = new JPanel(new GridLayout(1, 3));
+	    JLabel tekst2 = new JLabel("<html>"
+	    		+ "ANTS<br/>"
+	    		+ "Tanel Aamer<br/>"
+	    		+ "tanel.aamer@eesti.ee<br/>"
+	    		+ "Toomas Õunmaa, Umar Zarip, Tanel Aamer<br/>"
+	    		+ "TBA</html>");
+	    sisu2.add(tekst2);
+	    
+	    raam.add(logo);
+	    raam.add(tekst);
+	    raam.add(tekst2);
+
+	// 
+	         raam.setVisible(true); 
+
+	}
+}

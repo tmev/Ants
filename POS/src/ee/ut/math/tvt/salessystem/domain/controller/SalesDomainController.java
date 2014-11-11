d59da2885f077cd3364d2de141a7405a33de678b
tanel3203 <tanel3203@gmail.com>
2014-11-11 23:01:49 +0200

    Modisin SalesDomainController.java ja SalesDomainControllerImpl.java

--- POS/src/ee/ut/math/tvt/salessystem/domain/controller/SalesDomainController.java
+++ POS/src/ee/ut/math/tvt/salessystem/domain/controller/SalesDomainController.java
@@ -45,5 +45,5 @@ public interface SalesDomainController {
     public void submitCurrentPurchase(List<SoldItem> goods)
             throws VerificationFailedException;
 
-    
+    public void endSession();
 }
